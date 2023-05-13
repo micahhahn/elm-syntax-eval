b177b2ae4a4cda0b23305eea558a7c99f889b299
Micah Hahn <micah.s.hahn@gmail.com>
2023-05-13 16:01:31 -0500

    Add core types

--- src/Elm/Syntax/Eval.elm
+++ src/Elm/Syntax/Eval.elm
@@ -1,3 +1,34 @@
 module Elm.Syntax.Eval exposing (..)
 
+import Dict exposing (Dict)
+import Elm.Syntax.Expression exposing (Expression(..))
+import Elm.Syntax.Node as Node exposing (Node(..))
+import Elm.Syntax.Pattern exposing (Pattern(..))
+import List exposing (tail)
+
+
+type ElmValue
+    = ElmUnit
+    | ElmInt Int
+    | ElmFloat Float
+    | ElmString String
+    | ElmChar Char
+    | ElmList (List ElmValue)
+    | ElmTuple (List ElmValue)
+    | ElmRecord (Dict String ElmValue)
+    | ElmConstructor ConstructorName (List ElmValue)
+    | ElmLambda (ElmValue -> Result Error ElmValue)
+
+
+type ModuleName
+    = ModuleName String
+
+
+type ConstructorName
+    = ConstructorName ModuleName String
+
+
+type Error
+    = Error
+
 eval = Debug.todo ""
