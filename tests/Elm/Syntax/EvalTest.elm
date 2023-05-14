6cafef216915ae8a8091e0ebdb4d850caa717a59
Micah Hahn <micah.s.hahn@gmail.com>
2023-05-14 14:03:56 -0500

    Add expression testing harness

---
+++ tests/Elm/Syntax/EvalTest.elm
@@ -0,0 +1,60 @@
+module Elm.Syntax.EvalTest exposing (suite)
+
+import Elm.Parser exposing (parse)
+import Elm.Processing
+import Elm.Syntax.Declaration exposing (Declaration(..))
+import Elm.Syntax.Eval exposing (evalExpression, ElmValue(..))
+import Elm.Syntax.Expression exposing (Expression(..))
+import Elm.Syntax.Node as Node exposing (Node(..))
+import Expect
+import Dict
+import Test exposing (..)
+
+
+makeExpression : String -> Node Expression
+makeExpression elmExpression =
+    let
+        expressionName =
+            "loremipsum"
+
+        elmFile =
+            "module Test exposing (..)\n\n"
+                ++ expressionName
+                ++ " = "
+                ++ String.trim elmExpression
+
+        file =
+            case parse elmFile of
+                Err err ->
+                    Debug.todo ("Failed to parse file with: " ++ Debug.toString err)
+
+                Ok rawFile ->
+                    Elm.Processing.process Elm.Processing.init rawFile
+    in
+    case file.declarations of
+        [ Node _ declaration ] ->
+            case declaration of
+                FunctionDeclaration functionImplementation ->
+                    functionImplementation
+                        |> .declaration
+                        |> Node.value
+                        |> .expression
+
+                _ ->
+                    Debug.todo "Expected an expression node"
+
+        _ ->
+            Debug.todo "makeExpression should only contain an expresion"
+
+
+suite =
+    describe "Elm.Syntax.Eval"
+        [ test "Unit Expression" <|
+            \_ ->
+                """
+    ()
+                """
+                    |> makeExpression
+                    |> evalExpression Dict.empty
+                    |> Expect.equal (Ok ElmUnit)
+        ]
